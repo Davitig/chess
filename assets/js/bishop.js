91f42207878f7d2696e025b8ed33e2b0a350dfd4
Davit Gvritishvili <gvritishvili.davit@gmail.com>
2022-02-13 18:16:00 +0400

    Define bishop movable squares

--- assets/js/bishop.js
+++ assets/js/bishop.js
@@ -48,4 +48,93 @@ class Bishop {
     getFile() {
         return this[this.side].file;
     }
+
+    /**
+     * Define available squares for the peace.
+     *
+     * @param chess object
+     * @return array
+     */
+    defineMoves(chess) {
+        return this.getMovableSquares(chess);
+    }
+
+    /**
+     * Get movable squares.
+     *
+     * @param chess object
+     * @return array
+     */
+    getMovableSquares(chess) {
+        let squares = chess.getSquaresArray();
+        let squareKey = chess.getSquaresArrayKey(chess.activeSquare, squares);
+
+        let newSquares = [];
+
+        // get west north squares.
+        newSquares = this.getDiagonalSquares(squares, squareKey, (squareKey) => {
+            return squareKey - 9;
+        }, 'a');
+        // get north east squares.
+        newSquares = newSquares.concat(
+            this.getDiagonalSquares(squares, squareKey, (newSquareKey) => {
+                return newSquareKey - 7;
+            }, 'h')
+        );
+        // get east south squares.
+        newSquares = newSquares.concat(
+            this.getDiagonalSquares(squares, squareKey, (newSquareKey) => {
+                return newSquareKey + 9;
+            }, 'a')
+        );
+        // get south west squares.
+        newSquares = newSquares.concat(
+            this.getDiagonalSquares(squares, squareKey, (newSquareKey) => {
+                return newSquareKey + 7;
+            }, 'h')
+        );
+
+        return newSquares;
+    }
+
+    /**
+     * Get diagonal squares.
+     *
+     * @param squares array
+     * @param squareKey integer
+     * @param newSquareKeyFunc closure
+     * @param edgeAlphabet string
+     * @return array
+     */
+    getDiagonalSquares(squares, squareKey, newSquareKeyFunc, edgeAlphabet) {
+        let newSquares = [];
+        let newSquareKey = squareKey;
+
+        for (let i = 1; i <= 8; i++) {
+            newSquareKey = newSquareKeyFunc(newSquareKey);
+
+            let targetSquare = squares[newSquareKey];
+
+            if (targetSquare !== undefined) {
+                let targetSquarePeace = chess.squares[targetSquare];
+
+                newSquares.push(targetSquare);
+
+                // if object
+                if (targetSquarePeace.length === undefined) {
+                    if (targetSquarePeace.side === this.side) {
+                        newSquares.pop();
+                    }
+
+                    break;
+                }
+
+                if (chess.splitSquare(targetSquare)[0] === edgeAlphabet) {
+                    break;
+                }
+            }
+        }
+
+        return newSquares;
+    }
 }
